d1849e02059b4260f1392f4b13ce136e3f9a9afd
Atsushi Nagase <a@ngs.io>
2016-06-02 23:36:58 +0900

    Implement Realm Demo

---
+++ Podfile
@@ -0,0 +1,9 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'RealmNotificationDemo' do
+  use_frameworks!
+  inhibit_all_warnings!
+
+  pod 'RealmSwift', '~> 1.0.0'
+end
--- RealmNotificationDemo/ViewController.swift
+++ RealmNotificationDemo/ViewController.swift
@@ -7,19 +7,57 @@
 //
 
 import UIKit
+import RealmSwift
+
+class TestObject: Object {
+    dynamic var value: Double = 0.0
+    dynamic var key: String = "onlyme"
+
+    override static func primaryKey() -> String {
+        return "key"
+    }
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet var valueLabel: UILabel!
+    @IBOutlet var stepper: UIStepper!
+    var realm: Realm!
+    var results: Results<TestObject>!
+    var notificationToken: NotificationToken?
+
+    deinit {
+        notificationToken?.stop()
+        notificationToken = nil
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        realm = try! Realm(configuration: Realm.Configuration(inMemoryIdentifier: "test"))
+        results = realm.objects(TestObject)
+        notificationToken = results.addNotificationBlock { _ in
+            print("Notified!")
+        }
+        valueLabel.text = stepper.value.description
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func stepperValueChange(sender: AnyObject!) {
+        valueLabel.text = stepper.value.description
     }
 
+    @IBAction func saveButtonClick(sender: AnyObject!) {
+        realm.beginWrite()
+        if let obj = realm.objectForPrimaryKey(TestObject.self, key: "onlyme") {
+            obj.value = stepper.value
+        } else {
+            let obj = TestObject()
+            obj.key = "onlyme"
+            obj.value = stepper.value
+            realm.add(obj)
+        }
+        try! realm.commitWrite()
+    }
 
 }
 
